e0cfb2889c3182b0344bd13f9b2e6ec8b671a6a3
Brian886 <154473817+Brian886@users.noreply.github.com>
2025-05-17 12:00:00 +0200

    Add files via upload

--- Class 10/Practice Multivariate Linear Regression.R
+++ Class 10/Practice Multivariate Linear Regression.R
@@ -25,3 +25,99 @@ corrplot(corr_matrix, method="number")
 #----------------------------------------------------------------------------------------
 multvariatemodel <- lm(DeathsCOVID ~ Nurses+Unemp+WomenOlder65 ,data=covid)
 summary(multvariatemodel)
+
+
+#------------------------------------------------------------
+#3)Marginal Effect of Predictors
+#-----------------------------------------------------------
+summary(multvariatemodel)
+#beta1. +0.04
+#Covid mortality rate is expected to rise by 0.04% point when num of nurses
+#per 10,000 inhabitants increase by 1, ceteris paribus
+#beta2. +0.0028
+#beta3. +0.2656
+#beta0. -0.15
+
+#3.1) Partial elasticity
+Betas <- coef(multvariatemodel) #save the coefficients to a separate vector for further calculations
+Betas #first element is beta0
+
+#elasticity
+#unemployment on COVID mortality is in the Pécs district, 
+#where the number of nurses per 10,000 people is 6.5, 
+#unemployment is 222/10,000, and the proportion of women over 65 is 13.3%:
+elasticity <- (Betas[3]*222)/(Betas[1]+Betas[2]*6.5+Betas[3]*222+Betas[4]*13.3)
+elasticity
+#result is already in %
+#0.15%. 
+#if the 222 unemployment rate is increased by 1% and ceteris paribus, the
+#corvid mortality is expected to increase by 0.15%.
+
+#Let’s compute the unemployment elasticity in the Siklós district, 
+#where Nurses=6.7, Unemp=692, WomenOlder65=12.7:
+elasticity2 <- (Betas[3]*682)/(Betas[1]+Betas[2]*6.7+Betas[3]*692+Betas[4]*12.7)
+elasticity2
+
+#3.2)Partial correlation
+mean(covid$Nurses)
+mean(covid$Unemp)
+#Comparing βj
+#coefficients across such different averages would be unfair, 
+#since +1 unit is relatively more significant among nurses than
+#among unemployed people.
+
+#let us check with correlation
+cor(covid$DeathsCOVID, covid$Nurses)
+cor(covid$DeathsCOVID, covid$Unemp) 
+
+#we prefer partial correlations use ppcor library
+install.packages("ppcor") 
+library(ppcor)
+
+pcor(covid[,2:5]) # columns 2-5 contain variables in our regression
+#unemployment has a +0.383 partial correlation with COVID mortality 
+#after controlling for the effects of the number of nurses and 
+#the proportion of women over 65.
+#In contrast, the number of nurses has only a very weak partial 
+#correlation with COVID mortality, with an absolute value below 0.05 (0.047) 
+#after controlling for unemployment and the proportion of women over 65.
+
+#3.3) Path analysis
+#PredictDeathsCOVID=β1×Nurses+β2×Unemp+β0.
+## first, get the multivariate regression with Nurses and Unemp
+twopredictor_model <- lm(DeathsCOVID ~ Nurses+Unemp, data=covid)
+#get the coefficients
+twopredictor_model$coefficients
+#save the 2nd and 3rd elements in the vectors: note that the 1st element is Beta_0!
+Beta_nurses_covid <- twopredictor_model$coefficients[2]
+Beta_unemp_covid <- twopredictor_model$coefficients[3]
+
+#The magnitude of the effect on the green arrow is simply given by the coefficient β1
+#of the bivariate regression PredictUnemp=β1×Nurses+β0.
+# first, get the simple (bivariate regression)
+unemp_nurses_model <- lm(Unemp~Nurses, data=covid)
+# get the vector of coefficients
+unemp_nurses_model$coefficients
+# save the 2nd element in the vectors: note that the 1st element is Beta_0!
+Beta_unemp_nurses <- unemp_nurses_model$coefficients[2]
+
+#direct effect of nurses on COVID deaths (red arrow)
+direct_nurse_covid <- Beta_nurses_covid
+direct_nurse_covid
+
+## indirect effect of nurses on COVID deaths (green*blue arrow)
+indirect_nurse_covid <- Beta_unemp_nurses*Beta_unemp_covid
+indirect_nurse_covid
+
+# total effect = direct + indirect effects
+total_nurses_covid <- direct_nurse_covid+indirect_nurse_covid
+total_nurses_covid
+
+#value is exactly the same as the slope of the original simple bivariate regression PredictCOVIDDeaths=β1×Nurses+β0
+simple_model$coefficients
+
+
+#3.4) Partial t-test
+summary(multvariatemodel)
+# 0.096 means that the β of Nurses is 0.04 in the sample, 
+#but if we ran the regression on new districts outside the observed sample, this value could fluctuate around 0.04 with an expected variability of ±0.096
